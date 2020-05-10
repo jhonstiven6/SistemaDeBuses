4f8ed16c3dba92241b8cc7e4c1d6c8ef49793a94
JhonSanpedro <stiven.sanpedro@utp.edu.co>
2020-05-10 14:30:08 -0500

    inicializando repositorio

---
+++ src/main/java/co/edu/utp/SistemaDeBuses/Buses/SistemaDeControlParaBusesApplication.java
@@ -0,0 +1,13 @@
+package co.edu.utp.SistemaDeBuses.Buses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeControlParaBusesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeControlParaBusesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/utp/SistemaDeBuses/Buses/SistemaDeControlParaBusesApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.utp.SistemaDeBuses.Buses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeControlParaBusesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
